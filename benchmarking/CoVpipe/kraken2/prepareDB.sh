ac9d56631c432467492a5b40c624067706cc5f91
tobiasrausch <rauschtobi@gmail.com>
2021-02-07 19:31:10 +0100

    clean-up

--- benchmarking/CoVpipe/kraken2/prepareDB.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget -O GRCh38.p13_GBcovid19-2020-05-22.tar.gz  https://zenodo.org/record/3854856/files/GRCh38.p13_GBcovid19-2020-05-22.tar.gz?download=1
-tar -xzf GRCh38.p13_GBcovid19-2020-05-22.tar.gz
-rm GRCh38.p13_GBcovid19-2020-05-22.tar.gz
