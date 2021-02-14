066fc8cd24681b9101f607861200cf135d62f920
tobiasrausch <rauschtobi@gmail.com>
2021-02-14 21:43:49 +0100

    depth histogram

---
+++ scripts/depth.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+
+args=commandArgs(trailingOnly=TRUE)
+x = read.table(args[1], header=F)
+
+png(paste0(args[1], ".png"), width=1200, height=600)
+p = ggplot(data=x, aes(x=V1, y=V2))
+p = p + geom_line() + ylab("#Samples <10x") + xlab("Position")
+p = p + scale_x_continuous(breaks=(1:30*1000), limits=c(0,29903))
+p = p + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+p
+dev.off()
