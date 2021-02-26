79e7fb2ac471b5d605b8b8e439c2bea75769b210
tobiasrausch <rauschtobi@gmail.com>
2021-02-26 17:13:51 +0100

    contamination

--- scripts/contam.R
+++ scripts/contam.R
@@ -17,7 +17,7 @@ for (plate in unique(df$Plate)) {
     p =  p + geom_rect(aes(xmin=0, xmax=1, ymin=0, ymax=1, fill=Freemix))
     p = p + facet_grid(Row~Column)
     p = p + theme(legend.position="bottom", axis.text = element_blank(), axis.ticks = element_blank(), panel.grid  = element_blank())
-    p = p + labs(fill="Freemix") + xlab("") + ylab("")
+    p = p + labs(fill="Contamination") + xlab("") + ylab("")
     p = p + geom_text(aes(x=0.5, y=0.5, label=Freemix), vjust=-1)
     p = p + ggtitle(paste0(plate, " contamination estimates"))
     p = p + scale_fill_gradient2(limits=c(0, 1), mid="#f03b20", high="#bd0026", low="#f7f7f7", midpoint=0.5)
