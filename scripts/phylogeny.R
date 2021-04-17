c3a29603028e87193bb70cfed1ad9c444e597f92
tobiasrausch <rauschtobi@gmail.com>
2021-04-17 21:07:55 +0200

    dim

--- scripts/phylogeny.R
+++ scripts/phylogeny.R
@@ -3,6 +3,7 @@ library(ape)
 args = commandArgs(trailingOnly=TRUE)
 tree = read.tree(args[1])
 
-png(paste0(args[1], ".png"))
+#png(paste0(args[1], ".png"), height=4800, width=600)
+png(paste0(args[1], ".png"), height=1200, width=600)
 plot(tree,no.margin=TRUE,edge.width=2)
 dev.off()
