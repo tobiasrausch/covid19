3caa516694699de1fe14327874fe4493eca37753
tobiasrausch <rauschtobi@gmail.com>
2021-11-16 13:24:06 +0100

    nexclade datasets

--- src/lineage.sh
+++ src/lineage.sh
@@ -31,28 +31,16 @@ then
     then
 	# run nextclade container
 	cp ${FASTA} /tmp
-	if [ ! -f /tmp/reference.fasta ]
+	if [ ! -d /tmp/sars-cov-2 ]
 	then
-	    wget -O /tmp/reference.fasta https://raw.githubusercontent.com/nextstrain/nextclade/master/data/sars-cov-2/reference.fasta
-	fi
-	if [ ! -f /tmp/genemap.gff ]
-	then
-	    wget -O /tmp/genemap.gff https://raw.githubusercontent.com/nextstrain/nextclade/master/data/sars-cov-2/genemap.gff
-	fi
-	if [ ! -f /tmp/tree.json ]
-	then
-	    wget -O /tmp/tree.json https://raw.githubusercontent.com/nextstrain/nextclade/master/data/sars-cov-2/tree.json
-	fi
-	if [ ! -f /tmp/qc.json ]
-	then
-	    wget -O /tmp/qc.json https://raw.githubusercontent.com/nextstrain/nextclade/master/data/sars-cov-2/qc.json
+	    docker run -it --name nextclade --rm -u `id -u` --volume="/tmp/:/seq" nextstrain/nextclade nextclade dataset get --name sars-cov-2 --output-dir '/seq/sars-cov-2'
 	fi
 	if [ "$(docker ps -q -f name=nextclade)" ]
 	then
 	    docker rm -vf nextclade
 	    sleep 1
 	fi
-	docker run -it --name nextclade --rm -u `id -u` --volume="/tmp/:/seq" nextstrain/nextclade nextclade --input-fasta "/seq/${FASTA}" --input-root-seq "/seq/reference.fasta" --input-tree "/seq/tree.json" --input-gene-map "/seq/genemap.gff" --input-qc-config "/seq/qc.json" --output-basename ${OUTP} --output-dir "/seq/" --output-json /seq/${OUTP}.json --output-tsv /seq/${OUTP}.tsv
+	docker run -it --name nextclade --rm -u `id -u` --volume="/tmp/:/seq" nextstrain/nextclade nextclade run --input-fasta "/seq/${FASTA}" --input-dataset '/seq/sars-cov-2' --output-basename ${OUTP} --output-dir "/seq/" --output-json /seq/${OUTP}.json --output-tsv /seq/${OUTP}.tsv
 	cp /tmp/${OUTP}.json .
 	cp /tmp/${OUTP}.tsv .
 	rm /tmp/${OUTP}*
