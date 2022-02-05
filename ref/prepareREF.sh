885b4d0f775e0effbffdc6f4c7b2659f8c6f403d
tobiasrausch <rauschtobi@gmail.com>
2022-02-05 21:33:32 +0100

    vep

--- ref/prepareREF.sh
+++ ref/prepareREF.sh
@@ -16,5 +16,6 @@ samtools faidx Homo_sapiens.GRCh38.dna.primary_assembly.fa
 bwa index Homo_sapiens.GRCh38.dna.primary_assembly.fa
 
 # Precompute VEP for all single-nucleotide variants (already done)
+# Annotation resources: https://covid-19.ensembl.org/info/data/ftp/index.html
 # bcftools view NC_045512.2.anno.bcf | grep -v "^#" | cut -f 1-5 > ~/vep/input/input.vcf 
 # docker run -t -i -v /home/rausch/vep:/opt/vep/.vep ensemblorg/ensembl-vep ./vep --species sars_cov_2 --cache_version 101 --check_existing --transcript_version --no_stats --cache --offline --compress_output bgzip --format vcf --vcf --force_overwrite --dir_cache /opt/vep/.vep/ --dir_plugins /opt/vep/.vep/Plugins/ --input_file /opt/vep/.vep/input/input.vcf --output_file /opt/vep/.vep/output/out.vcf.gz
